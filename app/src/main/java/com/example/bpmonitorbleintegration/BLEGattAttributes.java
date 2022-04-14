ad9d3b695894ed768ac194dc6a35a709a0d4d6c6
Vivek Pratap <vivek.p@fueblabs.com>
2022-04-14 16:07:37 +0530

    BLE Example

--- app/src/main/java/com/example/bpmonitorbleintegration/BLEGattAttributes.java
+++ app/src/main/java/com/example/bpmonitorbleintegration/BLEGattAttributes.java
@@ -18,15 +18,4 @@ public class BLEGattAttributes {
         String name = attributes.get(uuid);
         return name;
     }
-//    private static HashMap<String,String> attributes = new HashMap();
-//    public static String CLIENT_CHARACTERISTIC_CONFIG = "0000ffe1-0000-1000-8000-00805f9b34fb";
-//
-////    static {
-////        attributes.put()
-////    }
-//
-//    public static String lookup(String rxDatum) {
-//        String name = attributes.get(uuid);
-//        return name;
-//    }
 }
