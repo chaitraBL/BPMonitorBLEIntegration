d0f80318a97db012465096b3ce3ac3b620b1682a
Vivek Pratap <vivek.p@fueblabs.com>
2022-04-15 15:47:08 +0530

    BLE Example

---
+++ app/src/main/java/com/example/bpmonitorbleintegration/Constants.java
@@ -0,0 +1,20 @@
+package com.example.bpmonitorbleintegration;
+
+public class Constants {
+    public final static String ACTION_GATT_CONNECTED =
+            "android-er.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "android-er.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "android-er.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "android-er.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "android-er.EXTRA_DATA";
+
+    public static final int STATE_DISCONNECTED = 0;
+    public static final int STATE_CONNECTING = 1;
+    public static final int STATE_CONNECTED = 2;
+
+    public static final byte[] startValue = {0x7B,0x00,0x00,0x00,0x01,0x10,0x0A,0x00,0x01,0x00,0x1C,0x7D};
+}
